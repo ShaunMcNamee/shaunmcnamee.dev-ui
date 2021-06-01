4fc3946cd26527cd681e2d728249a38edc67d0dd
Shaun McNamee <ShaunMcNamee+github@gmail.com>
2021-06-01 09:10:45 -0500

    refactor: head
    - add metadata stuff to head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,19 +4,31 @@ import { Header } from '../components/Header'
 import theme from '../../styles/theme'
 import { Footer } from '../components/Footer'
 
+const title = 'Shaun 2.0'
+const description = 'A web dev talking about the world around him.'
+
 function MyApp({ Component, pageProps }) {
-  return <ThemeProvider theme={theme}>
-    <CSSReset />
-    <Head>
-      <title>Shaun McNamee</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Stack dir="column" height="100vh" width="100vw">
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </Stack>
-  </ThemeProvider>
+  return (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Head>
+        <title>Shaun 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content={description} />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={description} />
+        <meta name="og:type" content="website" />
+        <meta name="twitter:creator" content="@shaunpmcnamee" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+      </Head>
+      <Stack dir="column" height="100vh" width="100vw">
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </Stack>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
