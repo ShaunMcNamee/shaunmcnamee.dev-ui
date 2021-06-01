7bc85fcf9f2f1f09d43db1109e8f6d02c2fcba04
Shaun McNamee <ShaunMcNamee+github@gmail.com>
2021-06-01 10:15:28 -0500

    refactor: header and footer
    - make header and footer look better on mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,19 @@ import React from 'react'
 export const Footer = () => (
   <Box margin="0 !important" as="footer" backgroundColor="brand.blueJeans">
     <Box m="0 auto" maxW="1200" px="6" py="5">
-      <Stack justify="space-between" align="center" flexDirection="row">
+      <Stack
+        justify="space-between"
+        align="center"
+        flexDirection={['column', 'row']}
+      >
         <Heading as="h4" size="md" color="brand.onyx">
           {`Shaun McNamee \u00A9 ${new Date().getFullYear()}`}
         </Heading>
-        <Stack align="center" flexDirection="row" margin="0 !important">
+        <Stack
+          align="center"
+          flexDirection={['column', 'row']}
+          margin="0 !important"
+        >
           <Link href="https://twitter.com/shaunpmcnamee" passHref>
             <ChakraLink variant="header">Twitter</ChakraLink>
           </Link>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,19 @@ export const Header = () => {
   return (
     <Box as="header" backgroundColor="brand.blueJeans">
       <Box m="0 auto" maxW="1200" px="6" py="5">
-        <Stack justify="space-between" align="center" flexDirection="row">
+        <Stack
+          justify="space-between"
+          align="center"
+          flexDirection={['column', 'row']}
+        >
           <Heading as="h1" m="0" color="brand.onyx">
             Shaun 2.0
           </Heading>
-          <Stack align="center" flexDirection="row" margin="0 !important">
+          <Stack
+            align="center"
+            flexDirection={['column', 'row']}
+            margin="0 !important"
+          >
             <Link href="/" passHref>
               <ChakraLink variant="header">Home</ChakraLink>
             </Link>
